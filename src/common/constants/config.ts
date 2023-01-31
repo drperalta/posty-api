2a232ee1cb6b84b071553592c4e8b017e8c9e553
David Peralta <peraltadavidr@outlook.com>
2023-01-31 10:22:36 +0800

    add config constants

---
+++ src/common/constants/config.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  GLOBAL_PREFIX: process.env.GLOBAL_PREFIX || 'api',
+  SWAGGER_PREFIX: process.env.SWAGGER_PREFIX || 'documentation',
+  PORT: process.env.PORT || 3333,
+};
