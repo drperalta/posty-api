0e3dc10272745fbf6b712f45902220364df97ed0
David Peralta <peraltadavidr@outlook.com>
2023-01-31 14:25:42 +0800

    generate resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
+import { ReactionsModule } from './reactions/reactions.module';
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, CommentsModule],
+  imports: [AuthModule, UsersModule, PostsModule, CommentsModule, ReactionsModule],
   controllers: [],
   providers: [],
 })
